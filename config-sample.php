8b5f6780caf81755ec2a7bfec705a0adca4dea26
J. Nathan Matias <natematias@gmail.com>
2022-03-23 12:15:55 +0000

    added code to export the follow network

--- config-sample.php
+++ config-sample.php
@@ -19,38 +19,43 @@ namespace OpenFuego;
  * http://php.net/manual/en/timezones.php
 **/
 date_default_timezone_set('America/New_York');
-	
+
 /** Time to curate.
   * Specify the authorities at the center of the universe.
   * Minimum 1, maximum 15. No @ symbol necessary.
  **/
 define(__NAMESPACE__ . '\AUTHORITIES', serialize(
 	array(
-		'someHandle1',
-		'someHandle2',
-		'someHandle3',
-		'someHandle4',
-		'someHandle5',
-		'someHandle6',
-		'someHandle7',
-		'someHandle8',
-		'someHandle9',
+		'', 
+		'', 
+		'',
+		'', 
+		'', 
+    '', 
+    '', 
+    '', 
+    '', 
+    '' 
 	)
 ));
 
 /** Your email address, for error reporting. Separate multiple addresses with commas. */
-const WEBMASTER = 'xxxx@harvard.edu';
+const WEBMASTER = '';
 
 /** For how many days should links remain in the database? Default: 1. Must be an integer. */
-const EXPIRATION_DAYS = 1;
+const EXPIRATION_DAYS = 120;
+
+/** Where should Fuego output audit trails for use by students in the class? */
+const OUTPUT_DATA = '';
+
 
 /** Your database credentials.
   * Only MySQL is supported, hipsters. */
 const
-	DB_NAME = 'xxxxx', 
-	DB_USER = 'xxxxx',
-	DB_PASS = 'xxxxx',
-	DB_HOST = 'localhost',  // default: localhost
+	DB_NAME = '',
+	DB_USER = '',
+	DB_PASS = '',
+	DB_HOST = '',  // default: localhost
 	DB_PORT = 3306;  // default: 3306
 
 /** Your Twitter credentials. If you don't have any yet, visit https://dev.twitter.com and sign in.
@@ -58,26 +63,33 @@ const
   * Then generate new access tokens. Don't share the secrets with anyone.
 **/
 const
-	TWITTER_SCREEN_NAME = 'xxxx',
-	TWITTER_CONSUMER_KEY = 'xxxx',
-	TWITTER_CONSUMER_SECRET = 'xxxx',
-	TWITTER_OAUTH_TOKEN = 'xxxx-xxxx',
-	TWITTER_OAUTH_SECRET = 'xxxx';
+	// TWITTER_SCREEN_NAME = 'AIGovNews',
+	// TWITTER_CONSUMER_KEY = 'XEz6tLv3DCF5d33Vh4FobMOO4',
+	// TWITTER_CONSUMER_SECRET = 'ebvjIVe7D2ZOu2TI30PjQfmpXgqzyOIF2M48qrOehWwr8ET4Vx',
+	// TWITTER_OAUTH_TOKEN = '1413595423-8uPRA3IPcLPrWcfGIszLW7H8R3XTuTVBZ8imXuj',
+	// TWITTER_OAUTH_SECRET = 'A82DfRZ1RPiYLoZkRfEQoaXsYQJxlLJ3MfGci65idE8z1';
+  TWITTER_SCREEN_NAME = '',
+  TWITTER_CONSUMER_KEY = '',
+  TWITTER_CONSUMER_SECRET = '',
+  TWITTER_OAUTH_TOKEN = '',
+  TWITTER_OAUTH_SECRET = '';
 
-/** Your Bitly credentials. Visit this page to grab them: http://bitly.com/a/your_api_key 
+/** Your Bitly credentials. Visit this page to grab them: http://bitly.com/a/your_api_key
   * Optional, recommended. Leave blank to disable.
 **/
 const
-	BITLY_USERNAME = '',
-	BITLY_API_KEY = '';
+	// BITLY_USERNAME = 'andrewphelps',
+	// BITLY_API_KEY = '754bbdbdcb1e79ed51cd91f8dc2a5c27b9b5aa3f';
+  BITLY_USERNAME = '',
+  BITLY_API_KEY = '';
 
 /** Your Goo.gl API credentials. Visit this page for instructions:
-  * https://developers.google.com/url-shortener/v1/getting_started#APIKey 
+  * https://developers.google.com/url-shortener/v1/getting_started#APIKey
   * Optional, recommended. Leave blank to disable.
 **/
 const GOOGL_API_KEY = '';
 
-/** Your Embed.ly API credentials. Visit this page for a free account: http://embed.ly/embed/pricing 
+/** Your Embed.ly API credentials. Visit this page for a free account: http://embed.ly/embed/pricing
   * Optional, but required to return metadata with URLs. Leave blank to disable.
 **/
 const EMBEDLY_API_KEY = '';
